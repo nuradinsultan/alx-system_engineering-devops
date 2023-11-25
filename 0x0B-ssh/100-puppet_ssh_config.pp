8718d021f73118c26e24d5ae9711c6516fe8e839
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-11-25 18:06:31 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# Puppet script to create ssh config file
+# Puppet script to a create ssh config file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
